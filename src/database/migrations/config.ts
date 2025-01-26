bd9d9b2b2a210d29791ae1fa8c94a932f86e9a5d
zlydenko <anton.zlydenko@gmail.com>
2025-01-27 00:45:22 +0200

    fix migrations config

--- src/database/migrations/config.ts
+++ src/database/migrations/config.ts
@@ -1,16 +1,17 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
+import { join } from 'path';
 
 config();
 
-export const AppDataSource = new DataSource({
+export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [],
-  migrations: ['src/storage/migrations/create-campaign-reports.ts'],
-  migrationsTableName: 'migrations'
+  entities: [join(__dirname, '..', 'entities', '*.entity.{ts,js}')],
+  migrations: [join(__dirname, './migration-*.{ts,js}')],
+  logging: true
 }); 
